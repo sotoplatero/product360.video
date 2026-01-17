7bcb7b0e7c2169a9bdddf4e92e7e0fd95313f30e
Damian Soto <soto.platero@gmail.com>
2026-01-17 10:51:44 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			// images: {
+			// 	sizes: [640, 828, 1200, 1920, 3840],
+			// 	formats: ['image/avif', 'image/webp'],
+			// 	minimumCacheTTL: 300,
+			// 	domains: ['360product.video'],
+			// },
+		})
 	},
 
 	preprocess: [mdsvex()],
