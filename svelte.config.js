a5fec14916799e6626d351e321ef64d1b96cff31
Damian Soto <soto.platero@gmail.com>
2026-01-17 10:53:13 -0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -7,16 +7,17 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			// images: {
-			// 	sizes: [640, 828, 1200, 1920, 3840],
-			// 	formats: ['image/avif', 'image/webp'],
-			// 	minimumCacheTTL: 300,
-			// 	domains: ['360product.video'],
-			// },
-		})
+		adapter: adapter()
+		// {
+		// default options are shown. On some platforms
+		// these options are set automatically — see below
+		// images: {
+		// 	sizes: [640, 828, 1200, 1920, 3840],
+		// 	formats: ['image/avif', 'image/webp'],
+		// 	minimumCacheTTL: 300,
+		// 	domains: ['360product.video'],
+		// },
+		// })
 	},
 
 	preprocess: [mdsvex()],
